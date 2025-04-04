d11ce12fe89093ff6d1740ebd25627d408fbf575
Daniel Gualtero <esneidergonzales2020@gmail.com>
2025-04-04 16:26:27 -0500

    add contantes de la pagina cotizar

---
+++ src/constants/cotizarConstants.js
@@ -0,0 +1,10 @@
+// constants.js
+export const COTIZACION_STEPS_TEXT = {
+    STEP_1: "Ingresa el departamento y ciudad donde se encuentra tu evento  ",
+    STEP_2: "Tambien los datos de destino para finalizar tu entrega",
+    STEP_3: "Elige el mes el dia y la hora de inicio de tu evento",
+    STEP_4: "Ingresa la cantidad y el articulo que llevaras para realizar tu evento. ¡ agregalos ! ",
+    STEP_5 : "Selecciona los servicios que utilizaras en esta ocasión",
+    STEP_6 : "Selecciona el medio por donde quieras enviar tu cotizacion . Tambien la puedes guardar en pdf",
+
+  };
